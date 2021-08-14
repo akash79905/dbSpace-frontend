18f566ba446fd08817803303a794f0267281590e
Akash jain <akash85119@gmail.com>
2021-08-15 00:35:21 +0530

    little changes

--- react/social desking/src/Components/Map.js
+++ react/social desking/src/Components/Map.js
@@ -1,14 +1,14 @@
 import OfficeMap from "./office";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 function Map() {
   const data = [
     {
       id: 1,
-      chairDirection: "south",
-      x: 0,
-      y: 0,
+      chairDirection: "east",
+      x: 1,
+      y: 1,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
@@ -20,9 +20,9 @@ function Map() {
     },
     {
       id: 2,
-      chairDirection: "south",
+      chairDirection: "east",
       x: 1,
-      y: 0,
+      y: 3,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         // {
@@ -40,9 +40,9 @@ function Map() {
     },
     {
       id: 3,
-      chairDirection: "south",
-      x: 2,
-      y: 0,
+      chairDirection: "east",
+      x: 1,
+      y: 5,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         {
@@ -58,9 +58,9 @@ function Map() {
     },
     {
       id: 4,
-      chairDirection: "south",
+      chairDirection: "west",
       x: 3,
-      y: 0,
+      y: 1,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
@@ -72,8 +72,8 @@ function Map() {
     {
       id: 5,
       chairDirection: "west",
-      x: 0,
-      y: 1,
+      x: 3,
+      y: 3,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
@@ -83,9 +83,9 @@ function Map() {
     },
     {
       id: 6,
-      chairDirection: "east",
-      x: 1,
-      y: 1,
+      chairDirection: "west",
+      x: 3,
+      y: 5,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
@@ -94,63 +94,131 @@ function Map() {
     },
     {
       id: 7,
-      chairDirection: "north-west",
-      x: 2,
-      y: 1,
-      equipments: [
-        { type: "desk", specification: "Simple desk" },
-        {
-          type: "cpu",
-          specification: "Dual core 2.4 GHz, 8 GB RAM, 512 GB HD",
-        },
-        { type: "monitor", specification: "HP V197 18.5-inch" },
-        { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
-        { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
-        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
-        { type: "drawer", specification: "Simple drawer" },
-      ],
-    },
-    {
-      id: 8,
-      chairDirection: "north-east",
-      x: 3,
+      chairDirection: "east",
+      x: 5,
       y: 1,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
+        // { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
         { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
+        // { type: "drawer", specification: "Simple drawer" },
+        { type: "Booking Status", specification: "Booked" },
+      ],
+    },
+    {
+      id: 8,
+      chairDirection: "east",
+      x: 5,
+      y: 3,
+      equipments: [
+        { type: "desk", specification: "Simple desk" },
+        // {
+        //   type: "cpu",
+        //   specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
+        // },
+        // { type: "monitor", specification: "HP V197 18.5-inch" },
+        // { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
+        // { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        // { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
+        // { type: "drawer", specification: "Simple drawer" },
+        { type: "Booking Status", specification: "Vacant" },
       ],
     },
     {
       id: 9,
-      chairDirection: "west",
-      x: 0,
-      y: 2,
+      chairDirection: "east",
+      x: 5,
+      y: 5,
       equipments: [
         { type: "desk", specification: "Simple desk" },
+        {
+          type: "cpu",
+          specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
+        },
+        { type: "monitor", specification: "HP V197 18.5-inch" },
+        { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
         { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
     {
       id: 10,
-      chairDirection: "east",
-      x: 1,
-      y: 2,
+      chairDirection: "west",
+      x: 7,
+      y: 1,
       equipments: [
         { type: "desk", specification: "Simple desk" },
-        { type: "monitor", specification: "HP V197 18.5-inch" },
-        { type: "desktop", specification: "HP CPU, keyboard and mouse" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
         { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
-        { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
     {
       id: 11,
-      chairDirection: "south-west",
-      x: 2,
-      y: 2,
+      chairDirection: "west",
+      x: 7,
+      y: 3,
+      equipments: [
+        { type: "desk", specification: "Simple desk" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
+        { type: "drawer", specification: "Simple drawer" },
+      ],
+    },
+    {
+      id: 12,
+      chairDirection: "west",
+      x: 7,
+      y: 5,
+      equipments: [
+        { type: "desk", specification: "Simple desk" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "drawer", specification: "Simple drawer" },
+      ],
+    },
+    {
+      id: 13,
+      chairDirection: "east",
+      x: 9,
+      y: 1,
+      equipments: [
+        { type: "desk", specification: "Simple desk" },
+        { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
+        // { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
+        { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
+        // { type: "drawer", specification: "Simple drawer" },
+        { type: "Booking Status", specification: "Booked" },
+      ],
+    },
+    {
+      id: 14,
+      chairDirection: "east",
+      x: 9,
+      y: 3,
+      equipments: [
+        { type: "desk", specification: "Simple desk" },
+        // {
+        //   type: "cpu",
+        //   specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
+        // },
+        // { type: "monitor", specification: "HP V197 18.5-inch" },
+        // { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
+        // { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        // { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
+        // { type: "drawer", specification: "Simple drawer" },
+        { type: "Booking Status", specification: "Vacant" },
+      ],
+    },
+    {
+      id: 15,
+      chairDirection: "east",
+      x: 9,
+      y: 5,
       equipments: [
         { type: "desk", specification: "Simple desk" },
         {
@@ -159,63 +227,44 @@ function Map() {
         },
         { type: "monitor", specification: "HP V197 18.5-inch" },
         { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
-        { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
         { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
         { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
     {
-      id: 12,
-      chairDirection: "south-east",
-      x: 3,
-      y: 2,
+      id: 16,
+      chairDirection: "west",
+      x: 11,
+      y: 1,
       equipments: [
         { type: "desk", specification: "Simple desk" },
-        {
-          type: "cpu",
-          specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
-        },
-        { type: "monitor", specification: "HP V197 18.5-inch" },
-        { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
-        { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
-        { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
+        { type: "phone", specification: "Cisco Phone IP 7960G/7940G" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
     {
-      id: 13,
-      chairDirection: "south-east",
-      x: 13,
-      y: 2,
+      id: 17,
+      chairDirection: "west",
+      x: 11,
+      y: 3,
       equipments: [
         { type: "desk", specification: "Simple desk" },
-        {
-          type: "cpu",
-          specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
-        },
-        { type: "monitor", specification: "HP V197 18.5-inch" },
-        { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
-        { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
-        { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
+        { type: "laptop", specification: "Laptop Dell Inspiron 15 5000" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
     {
-      id: 14,
-      chairDirection: "south-east",
-      x: 3,
-      y: 12,
+      id: 18,
+      chairDirection: "west",
+      x: 11,
+      y: 5,
       equipments: [
         { type: "desk", specification: "Simple desk" },
-        {
-          type: "cpu",
-          specification: "Dual core 2.4 GHz, 16 GB RAM, 256 GB HD",
-        },
-        { type: "monitor", specification: "HP V197 18.5-inch" },
-        { type: "keyboard", specification: "HP Ultrathin Wireless Keyboard" },
-        { type: "chair-na", specification: "817L Kare Ergonomic Office Chair" },
-        { type: "mouse", specification: "HP USB 2 Button Optical Mouse" },
+        { type: "chair", specification: "817L Kare Ergonomic Office Chair" },
         { type: "drawer", specification: "Simple drawer" },
       ],
     },
@@ -237,9 +286,9 @@ function Map() {
             }
             arr.push(t);
           });
+
           updateTables(arr);
         }}
-        onMove={console.log}
         editMode={true}
         showNavigator={true}
         horizontalSize={5}
